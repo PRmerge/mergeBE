2b1117524e20475efb351e12fb5ae0ae3f549a25
jeangho <jeangho293@naver.com>
2022-02-26 23:46:14 +0900

    Refactor model, repository
    모델 종속성 주입, repo 추상화

--- src/routes/users/intro/patch.ts
+++ src/routes/users/intro/patch.ts
@@ -1,11 +1,25 @@
-import { Router } from 'express';
+import { NextFunction, Request, Response, Router } from 'express';
 import { UserAuth } from '../../../middlewares/auth/user-auth';
-import {UserService} from '../../../services/users/application/service';
+import { UserService } from '../../../services/users/application/service';
 
 const router = Router();
 const userAuth = new UserAuth();
 const userService = new UserService();
 
-router.patch('/users/intro', userAuth.loginUserAuth, userService.saveUserIntro);
+// Todo: 사용자 인증 구현시 수정
+router.patch('/users/intro', async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { intro }: { intro: string } = req.body;
+    // const { githubIndex } = res.locals;
+    const response = await userService.saveUserIntro(intro, 84619866);
+
+    res.json({
+      data: response.intro
+    })
+  } catch (err) {
+    next(err);
+  }
+
+});
 
 export default router;

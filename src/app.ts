e608d61d35ad2e3f33015d55cb911e0833c61bf6
jeangho <jeangho293@naver.com>
2022-04-02 01:04:04 +0900

    Add cors

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import * as express from 'express';
 import * as morgan from 'morgan';
 import * as compression from 'compression';
 import * as cookieParser from 'cookie-parser';
+import * as cors from 'cors';
 import helmet from 'helmet';
 import 'dotenv/config';
 import { connectMysql, connectMongo } from './db';
@@ -15,6 +16,7 @@ class App {
     this.app.use(morgan('dev'));
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
+    this.app.use(cors({origin: true}));
     this.app.use(cookieParser());
     this.app.use(helmet());
     this.app.use(compression());
